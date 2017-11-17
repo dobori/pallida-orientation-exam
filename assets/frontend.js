d90266545287acb876fbf1c2248d63c9f4db1c58
Borbala Dobo <dobori@gmail.com>
2017-11-17 12:54:54 +0100

    feat(cars): searchong by plate No is ok

--- assets/frontend.js
+++ assets/frontend.js
@@ -1,6 +1,10 @@
 'use strict'
 
-var url = 'http://localhost:8080/search';
+var url = 'http://localhost:8080/';
+
+var SubmitButton = document.querySelector('#submit');    
+SubmitButton.addEventListener("click", submitClick);
+
 
 const methods = function ( methodType, filtering, callback) {
     const xhr = new XMLHttpRequest();
@@ -8,53 +12,45 @@ const methods = function ( methodType, filtering, callback) {
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4) {
-            console.log({ "resp":xhr.responseText});
+            console.log({ 'resp':xhr.responseText});
             callback(JSON.parse(xhr.responseText));
         };
     };
     
     xhr.send();
-    console.log("request sent")
+    console.log('request sent')
 };
 
-
-// const getWithVariable = (id, callback) => { 
-    //     methods('GET', '' + id, callback);
-    // }
     
-const getDatas = callback => {
-    methods('GET', '', callback);
-}
-
-
 const renderCars = function(carsData) {
     var neededDatas = carsData.data;
     var table = document.querySelector('table');
-
+        table.innerHTML='';   
     neededDatas.forEach( car => {
         let oneCarRow =  document.createElement('tr');
-            oneCarRow.innerHTML = '<td>'+ car.plate + '</td><td><button value=' + car.car_brand + ' class=\'cars_brand\'>' + car.car_brand + '</button></td><td>' + car.car_model + '</td><td>' + car.color + '</td><td>' + car.year + '</td>';
-            table.appendChild(oneCarRow);
+        oneCarRow.innerHTML = '<td>'+ car.plate + '</td><td><button value=' + car.car_brand + ' class=\'cars_brand\'>' + car.car_brand + '</button></td><td>' + car.car_model + '</td><td>' + car.color + '</td><td>' + car.year + '</td>';
+        table.appendChild(oneCarRow);
     });
-
-    var htmlBrandButtons = document.querySelectorAll('.cars_brand');
-    var buttons = Array.from(htmlBrandButtons);
-    var filteringBrand = '';
-        buttons.forEach(function(element){
-            element.addEventListener("click", function(){
-                filteringBrand =  element.value;
-                getBrandData(element.value);
-                table.innerHTML='';
-            });   
-        });
-    }
-    
-const getBrandData = function(value, callback) {
-    methods('GET', '/' + value, callback); 
+        
 }
 
-// const sendDatas = function(postData) {
-//     methods('POST', '', pass, postData)
-// }
+function submitClick() {
+    var plateNo = document.querySelector('input').value;
+    if (plateNo !== "") {
+        getPlateNo(plateNo, renderCars);
+    }
+}
+    
+const getDatas = callback => {
+    methods('GET', 'search', callback);
+}
+
+const getBrandData = function(value, callback) {
+    methods('GET', 'search/' + value, callback); 
+}
+
+const getPlateNo = (plateNo, callback) => { 
+    methods('GET', 'search?plate=' + plateNo, callback);
+}
 
 var data = getDatas(renderCars);
