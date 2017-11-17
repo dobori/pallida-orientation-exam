43ade5125946e5c16520f5992c3f0b0391449064
Borbala Dobo <dobori@gmail.com>
2017-11-17 11:09:56 +0100

    feat(cars): all cars are in a table

--- assets/frontend.js
+++ assets/frontend.js
@@ -1,8 +1,8 @@
 'use strict'
 
-var url = 'http://localhost:8080/';
+var url = 'http://localhost:8080/search';
 
-const methods = function ( methodType, filtering, callback, reqBody ) {
+const methods = function ( methodType, filtering, callback) {
     const xhr = new XMLHttpRequest();
     xhr.open( methodType, url + filtering );
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -13,23 +13,34 @@ const methods = function ( methodType, filtering, callback, reqBody ) {
         };
     };
 
-    let postData = null;
-    if (reqBody) {
-        postData = JSON.stringify(reqBody);    
-    };
-
-    xhr.send(postData);
+    xhr.send();
     console.log("request sent")
 };
 
-const getWithVariable = (id, callback) => { 
-    methods('GET', '' + id, callback);
-}
+
+// const getWithVariable = (id, callback) => { 
+//     methods('GET', '' + id, callback);
+// }
 
 const getDatas = callback => {
     methods('GET', '', callback);
 }
 
-const sendDatas = function(postData) {
-    methods('POST', '', pass, postData)
+const renderCars = function(carsData) {
+    var table = document.querySelector('table');
+
+    carsData.forEach( car => {
+        let oneCarRow =  document.createElement('tr');
+            oneCarRow.innerHTML = '<td>'+ car.plate + '</td><td>' + car.brand + '</td><td>' + car.car_model + '</td><td>' + car.color + '</td><td>' + car.year + '</td>';
+            table.appendChild(oneCarRow);
+    });
+    
 }
+
+
+
+// const sendDatas = function(postData) {
+//     methods('POST', '', pass, postData)
+// }
+
+var data = getDatas(renderCars);
